1047d646e59d8496859a8241d5e114d651a18772
thud.do <thuandohusk65@gmail.com>
2022-07-19 20:30:26 +0700

    ui browser mirror first commit

---
+++ app/src/main/java/com/abc/sreenmirroring/ui/browsermirror/BrowserMirrorActivity.kt
@@ -0,0 +1,19 @@
+package com.abc.sreenmirroring.ui.browsermirror
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abc.sreenmirroring.R
+import com.abc.sreenmirroring.base.BaseActivity
+import com.abc.sreenmirroring.databinding.ActivityBrowserMirrorBinding
+
+class BrowserMirrorActivity : BaseActivity<ActivityBrowserMirrorBinding>() {
+
+    override fun initBinding() = ActivityBrowserMirrorBinding.inflate(layoutInflater)
+
+    override fun initViews() {
+
+        }
+
+    override fun initActions() {
+    }
+}
