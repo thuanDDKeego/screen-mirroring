fb1e794b7a040f14dc0f143644bfe9d43be1320c
thud.do <thuandohusk65@gmail.com>
2022-07-19 15:30:07 +0700

    home ui first commit

---
+++ app/src/main/java/com/abc/sreenmirroring/ui/HomeActivity.kt
@@ -0,0 +1,17 @@
+package com.abc.sreenmirroring.ui
+
+import com.abc.sreenmirroring.base.BaseActivity
+import com.abc.sreenmirroring.databinding.ActivityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+
+    override fun initBinding() = ActivityHomeBinding.inflate(layoutInflater)
+
+    override fun initViews() {
+    }
+
+    override fun initActions() {
+    }
+}
