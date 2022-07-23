01754a9987a3564c7d172371e89961e3e08e78bf
QuocBao <quocbaovu15101996@gmail.com>
2022-07-23 11:32:47 +0700

    style: UI change pin code in setting

--- app/src/main/java/com/abc/sreenmirroring/ui/settings/SettingActivity.kt
+++ app/src/main/java/com/abc/sreenmirroring/ui/settings/SettingActivity.kt
@@ -1,11 +1,15 @@
 package com.abc.sreenmirroring.ui.settings
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.CompoundButton
 import com.abc.sreenmirroring.base.BaseActivity
 import com.abc.sreenmirroring.config.AppPreferences
 import com.abc.sreenmirroring.databinding.ActivitySettingBinding
+import com.abc.sreenmirroring.databinding.LayoutDialogChangePinCodeBinding
 import timber.log.Timber
 
 class SettingActivity : BaseActivity<ActivitySettingBinding>() {
@@ -21,7 +25,7 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
 
     override fun initViews() {
         binding.switchOnOffPinCode.isChecked = AppPreferences().isTurnOnPinCode == true
-
+        binding.txtPinCode.text = AppPreferences().pinCode
     }
 
     override fun initActions() {
@@ -29,11 +33,31 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
             AppPreferences().isTurnOnPinCode = isChecked
         }
         binding.llChangePinCode.setOnClickListener {
-
+            if (AppPreferences().isTurnOnPinCode == true) {
+                val dialog = LayoutDialogChangePinCodeBinding.inflate(layoutInflater, binding.root, true)
+                dialog.btnSaveNewPinCode.setOnClickListener {
+                    AppPreferences().pinCode = dialog.pinView.text.toString()
+                    binding.txtPinCode.text = AppPreferences().pinCode
+                    dialog.root.visibility = View.INVISIBLE
+                    hideKeyboard(this)
+                }
+                dialog.bgDialog.setOnClickListener {
+                    hideKeyboard(this)
+                    dialog.root.visibility = View.INVISIBLE
+                }
+            }
         }
         binding.btnBack.setOnClickListener {
             onBackPressed()
         }
+
+    }
+
+    private fun hideKeyboard(context: Context) {
+        val inputManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val v = (context as Activity).currentFocus ?: return
+        inputManager.hideSoftInputFromWindow(v.windowToken, 0)
     }
 
 }
