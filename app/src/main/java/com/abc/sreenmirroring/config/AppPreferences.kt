7d36ed0aa64e91bb00cb9824d9a7870b9fedad4a
thud.do <thuandohusk65@gmail.com>
2022-07-25 16:41:24 +0700

    style: add onboard theme

--- app/src/main/java/com/abc/sreenmirroring/config/AppPreferences.kt
+++ app/src/main/java/com/abc/sreenmirroring/config/AppPreferences.kt
@@ -1,7 +1,7 @@
 package com.abc.sreenmirroring.config
 
-class AppPreferences: Preferences(name = "app_preferences") {
-    var isTheFirstTimeUseApp by booleanPref(defaultValue = false)
+class AppPreferences : Preferences(name = "app_preferences") {
+    var isTheFirstTimeUseApp by booleanPref(defaultValue = true)
     var pinCode by stringPref(defaultValue = "0000")
     var isTurnOnPinCode by booleanPref(defaultValue = false)
 }
--- app/src/main/java/com/abc/sreenmirroring/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/abc/sreenmirroring/ui/splash/SplashActivity.kt
@@ -3,16 +3,24 @@ package com.abc.sreenmirroring.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.abc.sreenmirroring.R
+import com.abc.sreenmirroring.config.AppPreferences
 import com.abc.sreenmirroring.ui.home.HomeActivity
 import kotlinx.coroutines.*
 
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var job: Job
+    private var delayTime = 500L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (AppPreferences().isTheFirstTimeUseApp == true) {
+            setTheme(R.style.OnboardTheme)
+            delayTime = 3000L
+            AppPreferences().isTheFirstTimeUseApp = false
+        }
         job = CoroutineScope(Dispatchers.Main).launch {
-            delay(500)
+            delay(delayTime)
             startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
             finish()
         }
