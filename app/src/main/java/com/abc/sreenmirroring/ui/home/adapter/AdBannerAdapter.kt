9a226c7c2baca7ddc4506f2b5715a2feda57c5c8
thud.do <thuandohusk65@gmail.com>
2022-07-27 10:02:20 +0700

    style: wifi state at home screen

--- app/src/main/java/com/abc/sreenmirroring/ui/home/adapter/AdBannerAdapter.kt
+++ app/src/main/java/com/abc/sreenmirroring/ui/home/adapter/AdBannerAdapter.kt
@@ -5,24 +5,22 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.abc.sreenmirroring.ui.home.uiadbanner.AdBannerFragment1
-import com.abc.sreenmirroring.ui.home.uiadbanner.AdBannerFragment2
-import com.abc.sreenmirroring.ui.home.uiadbanner.AdBannerFragment3
 
 class AdBannerAdapter(val context: Context, fragmentManager: FragmentManager) :
     FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getItem(position: Int): Fragment {
-        return when (position) {
-            0 -> {
-                AdBannerFragment1()
-            }
-            1 -> {
-                AdBannerFragment2()
-            }
-            else -> {
-                AdBannerFragment3()
-            }
-
-        }
+//        return when (position) {
+//            0 -> {
+//                AdBannerFragment1()
+//            }
+//            1 -> {
+//                AdBannerFragment2()
+//            }
+//            else -> {
+//                AdBannerFragment3()
+//            }
+//        }
+        return AdBannerFragment1()
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
