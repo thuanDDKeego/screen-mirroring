cc0baef126cdb8a582fe755ef22e4806a2ebf9ef
thuan-dd <ducthuan232tn@gmail.com>
2022-07-30 00:28:13 +0700

    feat: splash animation

--- app/src/main/java/com/abc/mirroring/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/abc/mirroring/ui/splash/SplashActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.animation.AnimationSet
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.abc.mirroring.R
 import com.abc.mirroring.config.AppPreferences
+import com.abc.mirroring.databinding.ActivitySplashBinding
 import com.abc.mirroring.utils.FirebaseTracking
 import com.soft.slideshow.ads.AppOpenManager
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 
 
 class SplashActivity : AppCompatActivity() {
@@ -18,33 +22,41 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         if (AppPreferences().isTheFirstTimeUseApp == true) {
             setTheme(R.style.OnboardTheme)
-        }
-        else {
-            binding = ActivitySplashBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            setupSplashView()
-        }
-        AppOpenManager.instance?.showAdAtSplash(this@SplashActivity)
             FirebaseTracking.logOnBoardingShowed()
         } else {
+            binding = ActivitySplashBinding.inflate(layoutInflater)
+            setContentView(binding.root)
             FirebaseTracking.logSplashShowed()
-            AppOpenManager.instance?.showAdAtSplash(this@SplashActivity)
+            setupSplashView()
+            lifecycleScope.launchWhenStarted { delay(1600)
+                AppOpenManager.instance?.showAdAtSplash(this@SplashActivity)
+            }
         }
     }
 
     private fun setupSplashView() {
-        val  animMoveDown = AnimationUtils.loadAnimation(applicationContext,
-            R.anim.move_down)
-        val  animFadeIn = AnimationUtils.loadAnimation(applicationContext,
-            R.anim.fade_in)
+        val animMoveDown = AnimationUtils.loadAnimation(
+            applicationContext,
+            R.anim.move_down
+        )
+        val animFadeIn = AnimationUtils.loadAnimation(
+            applicationContext,
+            R.anim.fade_in
+        )
+
+        val animMoveRight = AnimationUtils.loadAnimation(
+            applicationContext,
+            R.anim.move_right
+        )
         val animationSet = AnimationSet(false)
         animationSet.addAnimation(animMoveDown)
         animationSet.addAnimation(animFadeIn)
-        binding.cardVieLogo.startAnimation(animationSet)
+        binding.cardViewLogo.startAnimation(animationSet)
+        binding.txtTitleSplash.startAnimation(animMoveRight)
+        binding.txtContentSplash.startAnimation(animFadeIn)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        job.cancel()
     }
 }
