334fc849e7823cbd30d7e072754ff4fbdb9ad912
QuocBao <quocbaovu15101996@gmail.com>
2022-08-01 17:35:29 +0700

    style: browser mirror

--- app/src/main/java/com/abc/mirroring/ui/browsermirror/BrowserMirrorActivity.kt
+++ app/src/main/java/com/abc/mirroring/ui/browsermirror/BrowserMirrorActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.*
 import android.net.Uri
-import android.net.wifi.WifiManager
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -71,7 +70,22 @@ class BrowserMirrorActivity : PermissionActivity<ActivityBrowserMirrorBinding>()
             binding.txtPinCode.visibility = View.GONE
             binding.txtSecurity.visibility = View.GONE
         }
-        binding.txtWifiName.text = getWifiName()
+        observerWifiState(object : onWifiChangeStateConnection {
+            override fun onWifiUnavailable() {
+                CoroutineScope(Dispatchers.Main).launch {
+                    binding.txtWifiName.text = getString(R.string.wifi_not_connected)
+                    binding.txtWifiName.setTextColor(R.color.draw_red)
+                }
+            }
+
+            override fun onWifiAvailable() {
+                CoroutineScope(Dispatchers.Main).launch {
+                    binding.txtWifiName.text = getString(R.string.wifi_connected)
+                    binding.txtWifiName.setTextColor(R.color.black)
+                }
+            }
+        })
+
     }
 
     override fun initActions() {
@@ -154,17 +168,6 @@ class BrowserMirrorActivity : PermissionActivity<ActivityBrowserMirrorBinding>()
             }
             else -> {}
         }
-    }
-
-    private fun getWifiName(): String {
-        val wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
-        val info = wifiManager.connectionInfo
-        val wifiName = info.ssid.replace("\"", "")
-        Timber.d("getWifiName $info -- wifiName: ${wifiName.isEmpty()}")
-        if (wifiName.isEmpty()) {
-            return "Wifi: Connected"
-        }
-        return wifiName
     }
 
     private fun setNewPortAndReStart() {
