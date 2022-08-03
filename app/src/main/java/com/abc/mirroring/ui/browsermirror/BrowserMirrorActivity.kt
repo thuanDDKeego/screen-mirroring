fbf71eb033f3d4f71764ae23e11b4cd6643a5d73
thud.do <thuandohusk65@gmail.com>
2022-08-03 09:54:52 +0700

    fix: - spannable text view

--- app/src/main/java/com/abc/mirroring/ui/browsermirror/BrowserMirrorActivity.kt
+++ app/src/main/java/com/abc/mirroring/ui/browsermirror/BrowserMirrorActivity.kt
@@ -324,7 +324,7 @@ fun AppCompatTextView.makeLinks(vararg links: Pair<String, View.OnClickListener>
             }
         }
         startIndexOfLink = this.text.toString().indexOf(link.first, startIndexOfLink + 1)
-//      if(startIndexOfLink == -1) continue // todo if you want to verify your texts contains links text
+        if (startIndexOfLink == -1) continue // todo if you want to verify your texts contains links text
         spannableString.setSpan(
             clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
             Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
