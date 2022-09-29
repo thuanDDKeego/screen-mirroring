b9424ae1d1218196e919c121cc6852bbfb105691
thomas.do_asuszenbook <thuandohusk65@gmail.com>
2022-09-29 10:55:42 +0700

    - fix: remove appLovin from code - fix: ui home screen

--- app/src/main/java/com/abc/mirroring/config/AppConfigRemote.kt
+++ app/src/main/java/com/abc/mirroring/config/AppConfigRemote.kt
@@ -6,11 +6,4 @@ class AppConfigRemote() :
     PreferencesAdapterRC(name = "app_config_remote", devMode = BuildConfig.DEBUG) {
     var isUsingAdsOpenApp by booleanPref(defaultValue = true)
     var timeBetweenTwoAdsOpenAppShow by intPref(defaultValue = 10000)
-
-//    var isUsingAdmobGoMirrorDevice by booleanPref(defaultValue = false)
-//    var isUsingAdmobHomeOnboarding by booleanPref(defaultValue = false)
-//    var isUsingAdmobBackFromTutorial by booleanPref(defaultValue = false)
-//    var isUsingAdmobNative by booleanPref(defaultValue = false)
-
-//    var isUsingAdmobBrowserMirrorReward by booleanPref(defaultValue = false)
 }
