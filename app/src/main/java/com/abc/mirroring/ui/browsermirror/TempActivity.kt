6abf957179eed1f78d6d29dc8ed640669e26b7a6
thomas.do_asuszenbook <thuandohusk65@gmail.com>
2022-10-01 10:52:57 +0700

    - feat: turn on/off remote config ads -fix comment

---
+++ app/src/main/java/com/abc/mirroring/ui/browsermirror/TempActivity.kt
@@ -0,0 +1,23 @@
+package com.abc.mirroring.ui.browsermirror
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import kotlin.system.exitProcess
+
+class TempActivity: Activity() {
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, TempActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finishAffinity()
+        exitProcess(0)
+    }
+}
