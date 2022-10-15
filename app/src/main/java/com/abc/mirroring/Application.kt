529088db07797520478ae47034d6b530bd5bca3d
thomas.do_asuszenbook <thuandohusk65@gmail.com>
2022-10-15 14:08:22 +0700

    ui: bg premium halloween

--- app/src/main/java/com/abc/mirroring/Application.kt
+++ app/src/main/java/com/abc/mirroring/Application.kt
@@ -35,7 +35,7 @@ class Application : Application() {
         AppOpenManager.instance?.init(this, this.getString(AdType.APP_OPEN.adsId))
         AppOpenManager.instance?.disableAddWithActivity(AdActivity::class.java)
         AppOpenManager.instance?.disableAddWithActivity(SplashActivity::class.java)
-        val testDeviceIds = Arrays.asList("68DDC733EF933C7728111563202D9BB6", "F394E8C18C4B32FBB0CD9CBAF0241AE8")
+        val testDeviceIds = Arrays.asList("DBA790F3D127D7C4422A8B5E2EBA705B", "F394E8C18C4B32FBB0CD9CBAF0241AE8")
         val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
         MobileAds.setRequestConfiguration(configuration)
     }
