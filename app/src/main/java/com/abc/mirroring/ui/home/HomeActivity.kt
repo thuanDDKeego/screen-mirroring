6ac07a82966b4dda4dbba66a3983eacfb668025e
thuanpt <thuanpt@oshot.one>
2022-12-09 15:56:29 +0700

    update:add_fab_draggable_sale_off

--- app/src/main/java/com/abc/mirroring/ui/home/HomeActivity.kt
+++ app/src/main/java/com/abc/mirroring/ui/home/HomeActivity.kt
@@ -18,7 +18,6 @@ import com.abc.mirroring.ads.AppOpenManager
 import com.abc.mirroring.base.BaseActivity
 import com.abc.mirroring.cast.MainActivity
 import com.abc.mirroring.cast.MainActivity.Companion.MEDIA_ROUTE
-import com.abc.mirroring.cast.shared.cast.Caster
 import com.abc.mirroring.cast.shared.route.MediaRoute
 import com.abc.mirroring.config.AppConfigRemote
 import com.abc.mirroring.config.AppPreferences
@@ -52,8 +51,6 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
     @Inject
     lateinit var admobHelper: AdmobHelper
 
-    @Inject
-    lateinit var caster: Caster
 
     companion object {
         fun newIntent(context: Context): Intent {
@@ -94,7 +91,6 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
         //set swift mode with floating tools state
 //        binding.switchModeFloatingTool.isChecked = FloatToolService.isRunning
         initAnim()
-
         binding.imgPremium.visibility =
             if (AppConfigRemote().enable_premium == true) View.VISIBLE else View.GONE
     }
@@ -173,21 +169,6 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
 
     @SuppressLint("ClickableViewAccessibility")
     override fun initActions() {
-        // Start server streaming
-        CoroutineScope(Dispatchers.IO).launch {
-            caster.start().also { Timber.i("Caster initialized") }
-            caster.discovery.device.collect {
-                if (it == null) return@collect
-
-                binding.imgCast.setImageDrawable(
-                    ContextCompat.getDrawable(
-                        this@HomeActivity,
-                        if (it.isConnected) R.drawable.ic_cast_connected else R.drawable.ic_cast
-                    )
-                )
-            }
-        }
-
         goToMirrorActivityResult =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
                 val result = activityResult.resultCode
@@ -261,9 +242,8 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
         binding.imgPremium.setOnClickListener {
             startActivity(Intent(this@HomeActivity, PremiumActivity::class.java))
         }
-        binding.imgCast.setOnClickListener {
-            // TODO: open dialog
-            caster.discovery.picker(this)
+        binding.imgSaleOffFab.setOnClickListener {
+            Toast.makeText(this, "Sale off Onclick", Toast.LENGTH_SHORT).show()
         }
         castOnClickSection()
     }
@@ -306,7 +286,8 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
                     dialogCenter.showDialog(DialogCenter.DialogType.TooManyAds {
                         startActivity(intent)
                     })
-                } else startActivity(intent)
+                }
+                else startActivity(intent)
             }
         } else {
             startActivity(intent)
@@ -382,7 +363,6 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
         super.onDestroy()
         Timber.d("destroy home")
         dialogCenter.onDestroy()
-        caster.shutdown()
 //        countDownJob?.cancel()
     }
 }
