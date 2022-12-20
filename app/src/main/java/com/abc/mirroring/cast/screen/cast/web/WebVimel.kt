b824b04eed40f09a93c2a9d6f4698bd18ec3df2b
thomas.do_asuszenbook <thuandohusk65@gmail.com>
2022-12-20 15:41:42 +0700

    -fix: ui web cast - change jsoup.connect to onloadResource

--- app/src/main/java/com/abc/mirroring/cast/screen/cast/web/WebVimel.kt
+++ app/src/main/java/com/abc/mirroring/cast/screen/cast/web/WebVimel.kt
@@ -3,16 +3,15 @@ package com.abc.mirroring.cast.screen.cast.web
 import android.annotation.SuppressLint
 import android.content.Context
 import android.webkit.WebView
-import androidx.lifecycle.viewModelScope
-import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.launch
 import com.abc.mirroring.cast.State
 import com.abc.mirroring.cast.VimelStateHolder
+import com.abc.mirroring.cast.section.MediaType
 import com.abc.mirroring.cast.section.WebMedia
 import com.abc.mirroring.cast.shared.cast.Caster
 import com.abc.mirroring.cast.shared.cast.Command
-import com.abc.mirroring.cast.shared.cast.webcast.WebViewExtractor
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -23,7 +22,6 @@ class WebVimel @Inject constructor(
     @ApplicationContext val context: Context,
     var caster: Caster
 ) : VimelStateHolder<WebVimel.WebVimelState>(WebVimelState()) {
-
     companion object {
         const val GOOGLE_URL = "https://www.google.com/"
         const val SEARCH_URL = "https://www.google.com/search?q="
@@ -42,26 +40,46 @@ class WebVimel @Inject constructor(
     fun search(query: String) {
         webView?.loadUrl("$SEARCH_URL$query")
     }
+
     private fun onUrlChanged(url: String) {
         update { state -> state.copy(url = url) }
+        update { state -> state.copy(medias = listOf()) }
     }
 
-    private fun extract(url: String) {
-        viewModelScope.launch {
-            WebViewExtractor.streamableFromUrl(url).let {
-                update { state ->
-                    state.copy(medias = it)
-                }
-            }
-        }
+//    private fun extract() {
+//        viewModelScope.launch {
+//            webView?.let {
+//                WebViewExtractor.streamableFromUrl(it).let {
+//                    it.collect { media ->
+//                        val newMedias =
+//                            mutableListOf<WebMedia>()
+//                                .also { it.add(media) }
+//                                .also { it.addAll(state.value.medias) }
+//
+//                        update { state -> state.copy(medias = newMedias) }
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+    fun onLoadResource(url: String) {
+        if (!url.isMediaLink()) return
+        val media = url.toWebMedia()
+        val newMedias =
+            mutableListOf<WebMedia>()
+                .also { it.add(media) }
+                .also { it.addAll(state.value.medias) }
+        update { state -> state.copy(medias = newMedias) }
     }
 
     fun onPageFinished(view: WebView?, url: String?) {
         Timber.d("onPageFinished $url $webView")
         webView = view
+
         if (url != null) {
             onUrlChanged(url)
-            extract(url)
+//            webView?.let { extract() }
         }
     }
 
@@ -70,6 +88,29 @@ class WebVimel @Inject constructor(
             is Command.Previous -> webView?.goBack()
             is Command.Next -> webView?.goForward()
             else -> {}
+        }
+    }
+
+    private fun String.isMediaLink(): Boolean {
+        return this.let { str ->
+            val etx: String = str.split(".").last()
+            //this is full extension
+//            etx == "mp4" || etx == "flv" || etx == "m4a" || etx == "3gp" || etx == "mkv" ||
+//                    etx == "mp3" || etx == "ogg" || etx == "jpg" || etx == "png" || etx == "gif"
+            //this is extension that is supported by connect-sdk
+            etx == "mp4" || etx == "mp3" || etx == "png" || etx == "jpg"
+        }
+    }
+
+    private fun String.toWebMedia(): WebMedia {
+        return this.let { url ->
+            val etx: String = url.split(".").last()
+            val mediaType = when (etx.lowercase()) {
+                "mp4", "flv", "m4a", "3gp", "mkv" -> MediaType.Video
+                "jpg", "png", "gif" -> MediaType.Image
+                else -> MediaType.Audio
+            }
+            WebMedia(url, url, 0L, mediaType, etx)
         }
     }
 }
--- app/src/main/java/com/abc/mirroring/cast/screen/cast/web/web_cast_.kt
+++ app/src/main/java/com/abc/mirroring/cast/screen/cast/web/web_cast_.kt
@@ -10,26 +10,31 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.BottomSheetState
 import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AudioFile
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.HighQuality
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.VideoCall
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -43,29 +48,30 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
-import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import dev.sofi.extentions.SofiBinding
-import dev.sofi.extentions.SofiComponent
-import kotlinx.coroutines.launch
 import com.abc.mirroring.cast.GlobalVimel
 import com.abc.mirroring.cast.screen.cast.audible.AudibleParameter
 import com.abc.mirroring.cast.screen.cast.image.ImageParameter
-import com.abc.mirroring.destinations.audible_player_Destination
-import com.abc.mirroring.destinations.image_player_Destination
 import com.abc.mirroring.cast.section.MediaType
 import com.abc.mirroring.cast.section.SourceType
 import com.abc.mirroring.cast.section.Streamable
 import com.abc.mirroring.cast.shared.cast.Command
 import com.abc.mirroring.cast.shared.ui.component.intercepted_browser
 import com.abc.mirroring.cast.shared.ui.component.top_bar_webview
+import com.abc.mirroring.destinations.audible_player_Destination
+import com.abc.mirroring.destinations.image_player_Destination
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import dev.sofi.extentions.SofiBinding
+import dev.sofi.extentions.SofiComponent
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Destination
@@ -75,13 +81,14 @@ fun web_cast_(
     @SofiBinding main: GlobalVimel,
     vm: WebVimel = hiltViewModel()
 ) {
-    val context = LocalContext.current
+    val activity = LocalContext.current as Activity
 
     val state by vm.state.collectAsState()
 
     //value state of search bar (text-field)
     val txtSearch = remember { mutableStateOf("") }
-    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed))
+    val bottomSheetScaffoldState =
+        rememberBottomSheetScaffoldState(bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed))
     val scope = rememberCoroutineScope()
 
     fun toggleSheet(expand: Boolean) {
@@ -92,7 +99,7 @@ fun web_cast_(
     }
 
     fun customBackPressed() {
-        if (vm.isOriginalUrl()) (context as Activity).finish() else vm.onControl(Command.Previous)
+        if (vm.isOriginalUrl()) (activity).finish() else vm.onControl(Command.Previous)
         toggleSheet(false)
     }
 
@@ -101,7 +108,7 @@ fun web_cast_(
     }
 
     LaunchedEffect(state.medias) {
-        if (state.medias.isNotEmpty()) toggleSheet(expand = true)
+//        if (state.medias.isNotEmpty()) toggleSheet(expand = true) //TODO show size of medias noti, or shake button, or both
     }
 
     BackHandler {
@@ -113,8 +120,14 @@ fun web_cast_(
         sheetElevation = 4.dp,
         sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
         sheetPeekHeight = 0.dp,
+        sheetBackgroundColor = Color(0xFFF3F3F3),
         sheetContent = {
-            _bottom_sheet_part(medias = state.medias) { medias, current ->
+            _bottom_sheet_part(medias = state.medias,
+                onCollapse = {
+                    scope.launch {
+                        bottomSheetScaffoldState.bottomSheetState.collapse()
+                    }
+                }) { medias, current ->
                 if (main.state.value.isDeviceConnected) {
                     when (current.mediaType()) {
                         MediaType.Image -> navigator.navigate(
@@ -139,10 +152,12 @@ fun web_cast_(
                             )
                         )
                     }
-                } else main.caster.discovery.picker(context as Activity)
+                } else main.caster.discovery.picker(activity as Activity)
             }
         },
         backgroundColor = MaterialTheme.colorScheme.background,
+        floatingActionButton = {
+        },
         scaffoldState = bottomSheetScaffoldState
     ) {
         Column(
@@ -151,17 +166,24 @@ fun web_cast_(
                 .fillMaxSize()
         ) {
             //header: contains back button, premium, cast icon and searchbar
-            top_bar_webview(modifier = Modifier.fillMaxWidth(), navigator = navigator, onBack = { customBackPressed() }, txtSearch = txtSearch, onSearch = { vm.search(txtSearch.value) })
+            top_bar_webview(
+                modifier = Modifier.fillMaxWidth(),
+                navigator = navigator,
+                onBack = { customBackPressed() },
+                txtSearch = txtSearch,
+                onSearch = { vm.search(txtSearch.value) })
             //region Webview section
             intercepted_browser(
                 modifier = Modifier.weight(1f),
-                url = WebVimel.GOOGLE_URL,
-                onPageFinished = vm::onPageFinished
+                url = state.url,
+                onPageFinished = vm::onPageFinished,
+                onLoadResource = vm::onLoadResource
             )
             //endregion
 
             _footer(
-                onHomePressed = { navigator.popBackStack() },
+                onHomePressed = { activity.finish() },
+                vm = vm,
                 onQualityPressed = { toggleSheet(expand = true) }
             )
         }
@@ -170,11 +192,12 @@ fun web_cast_(
 
 @SofiComponent(useFor = ["web_view_"])
 @Composable
-@Preview(showBackground = true)
 private fun _footer(
     onHomePressed: () -> Unit = {},
+    vm: WebVimel,
     onQualityPressed: () -> Unit = {}
 ) {
+    val state by vm.state.collectAsState()
     Row(
         Modifier
             .fillMaxWidth()
@@ -183,17 +206,32 @@ private fun _footer(
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         IconControl(icon = Icons.Rounded.Home,
-            size = 24.dp,
+            size = 28.dp,
             modifier = Modifier
                 .clickable {
                     onHomePressed.invoke()
                 })
-        IconControl(icon = Icons.Rounded.HighQuality,
-            size = 24.dp,
-            modifier = Modifier
-                .clickable {
-                    onQualityPressed.invoke()
-                })
+        Row(verticalAlignment = Alignment.Top) {
+            IconControl(icon = Icons.Rounded.HighQuality,
+                size = 28.dp,
+                modifier = Modifier
+                    .clickable {
+                        onQualityPressed.invoke()
+                    })
+            if (state.medias.isNotEmpty()) {
+                Text(
+                    text = "${state.medias.size}",
+                    modifier = Modifier
+                        .size(16.dp)
+                        .clip(RoundedCornerShape(4.dp))
+                        .background(Color.Red)
+                        .aspectRatio(1f),
+                    textAlign = TextAlign.Center,
+                    color = Color.White,
+                    fontSize = 10.sp
+                )
+            }
+        }
     }
 }
 
@@ -201,24 +239,48 @@ private fun _footer(
 fun _bottom_sheet_part(
     modifier: Modifier = Modifier,
     medias: List<Streamable>,
+    onCollapse: () -> Unit,
     onItemPressed: (List<Streamable>, Streamable) -> Unit
 ) {
-
-    Column(
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    Box(
         modifier = modifier
-            .wrapContentHeight()
+            .heightIn(max = screenHeight*0.6f)
             .fillMaxWidth()
     ) {
-        Text(modifier = Modifier.padding(8.dp), text = "Quality options")
-        //for each 3 type of media
-        listOf(MediaType.Video, MediaType.Image, MediaType.Audio).forEach { type ->
-            //label follow media type
-            Text(modifier = Modifier.padding(8.dp), text = if (type == MediaType.Video) "Video" else if (type == MediaType.Image) "Image" else "Audio")
-            medias.filter { media -> media.mediaType() == type }.apply {
-                //list media after filter follow media type
-                forEach { item ->
-                    _option_item(media = item) {
-                        onItemPressed(this, item)
+        IconControl(icon = Icons.Rounded.Close,
+            size = 24.dp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.TopEnd)
+                .clickable {
+                    onCollapse.invoke()
+                })
+        Column(
+            modifier = Modifier
+                .wrapContentHeight()
+        ) {
+            Text(modifier = Modifier.padding(8.dp), text = "Quality options")
+            //for each 3 type of media
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState()),
+            ) {
+                listOf(MediaType.Video, MediaType.Image, MediaType.Audio).forEach { type ->
+                    //label follow media type
+                    Text(
+                        modifier = Modifier.padding(8.dp),
+                        text = if (type == MediaType.Video) "Video" else if (type == MediaType.Image) "Image" else "Audio"
+                    )
+                    medias.filter { media -> media.mediaType() == type }.apply {
+                        //list media after filter follow media type
+                        forEach { item ->
+                            _option_item(media = item) {
+                                onItemPressed(this, item)
+                            }
+                        }
                     }
                 }
             }
@@ -228,7 +290,11 @@ fun _bottom_sheet_part(
 
 
 @Composable
-fun _option_item(modifier: Modifier = Modifier, media: Streamable, onPressItem: (Streamable) -> Unit) {
+fun _option_item(
+    modifier: Modifier = Modifier,
+    media: Streamable,
+    onPressItem: (Streamable) -> Unit
+) {
 
     Box(
         modifier = modifier
